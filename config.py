b09d7c585738563ed3ff76dd805e68744d6bc7e0
Alonehubb <momovag764@yahoo.com>
2024-10-17 17:07:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "api.shareus.io") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "hRPS5vvZc0OGOEUQJMJzPiojoVK2") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "linkpays.in") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "e27949c396504dca50e6caed52b70917f00593c9") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/hhsoccso8853") # how to open link 
 
 # Website Info:
